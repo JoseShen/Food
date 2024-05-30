9e5899b0eb4d7558966266e461dd54c6e8d3e74f
Jose Shen <jose.shen@ryerson.ca>
2024-05-30 17:36:41 -0400

    hi

---
+++ gpt.py
@@ -0,0 +1,37 @@
+
+from openai import AsyncOpenAI
+import os
+from dotenv import load_dotenv
+load_dotenv()
+
+client = AsyncOpenAI()
+client.api_key = os.getenv("OPENAI_API_KEY")
+
+async def get_chatgpt_response(prompt: str) -> str:
+    response = await client.chat.completions.create(
+        model="gpt-4o",
+        messages=[
+            {"role": "system", "content": "You are a master chef that knows every recipe. Reply with formatted discord markdown, use headings and sub headings. Make sure to only respond to messages about food and nothing else." },
+            {"role": "user", "content": prompt}
+            ],
+        temperature=0.7
+    )
+
+    return response.choices[0].message.content
+
+async def get_chatgpt_image_response(image_url):
+    response = await client.chat.completions.create(
+        model = "gpt-4-turbo",
+        messages =[
+            {"role": "system", "content": "You are a master chef that knows every recipe. Reply with formatted discord markdown, use headings and sub headings. If the image does not look look like a food, tell the user that they can't eat that."},
+            {"role": "user", "content": [
+                    {"type": "text", "text": "How do I make this/these dish(es)?"},
+                    {"type": "image_url", "image_url": {"url": image_url} }
+                    ]
+            }
+        ]
+    )
+
+    return response.choices[0].message.content
+
+
---
+++ main.py
@@ -0,0 +1,95 @@
+from typing import Final 
+import base64
+import os
+from dotenv import load_dotenv
+from discord import Intents, Message, Embed
+from discord.ext import commands
+from gpt import get_chatgpt_response, get_chatgpt_image_response
+
+load_dotenv()
+
+# Get the Discord bot token from environment variables
+DISCORD_BOT_TOKEN: Final[str] = os.getenv('DISCORD_BOT_TOKEN')
+
+
+
+intents: Intents = Intents.default()
+intents.message_content = True 
+bot = commands.Bot(command_prefix="!", intents=intents)
+
+@bot.event 
+async def on_ready() -> None:
+    print(f"Logged in as {bot.user}")
+
+@bot.event
+async def on_message(message: Message) -> None:
+    if message.author == bot.user: # prevents bot from talking to itself
+        return
+
+    await bot.process_commands(message) # Processes commands in chat
+
+@bot.command()
+async def ask(ctx: commands.Context) -> None:
+    
+    await ctx.typing() # Shows that the bot is typing
+
+    if ctx.message.content == "!ask":
+        return await ctx.reply("Please provide a prompt.")
+    
+    prompt = ctx.message.content[6:]
+    response = await get_chatgpt_response(prompt)  
+
+    if response is not None:
+        # Handles large responses from ChatGPT
+        if len(response) > 4096:
+            chunks = split_response(response)
+            for chunk in chunks:
+                embed = Embed(description=chunk, color=0x00ff00)
+                await ctx.reply(embed=embed)
+        else:
+            embed = Embed(description=response, color=0x00ff00) # Create an embed with the ChatGPT response
+            await ctx.reply(embed=embed) # Reply to user with the response
+    else:
+        await ctx.reply("No response from ChatGPT.") # Handle the case when response is None
+    
+
+
+
+@bot.command()
+async def upload(ctx: commands.Context) -> None:
+    await ctx.typing()
+
+    if ctx.message.attachments:
+        # Get the URLs of the message
+        attachment_urls = [attachment.url for attachment in ctx.message.attachments]
+
+       
+        response = await get_chatgpt_image_response(attachment_urls[0])
+
+        if response is not None:
+            # Handles large responses from ChatGPT
+            if len(response) > 4096:
+                chunks = split_response(response)
+                for chunk in chunks:
+                    embed = Embed(description=chunk, color=0x00ff00)
+                    await ctx.reply(embed=embed)
+            else:
+                embed = Embed(description=response, color=0x00ff00)  # Create an embed with the ChatGPT response
+                await ctx.reply(embed=embed)  # Reply to user with the response
+        else:
+            await ctx.reply("No response from ChatGPT.")  # Handle the case when response is None
+
+    else:
+        return await ctx.reply("No attachments found, upload an image of a food.")
+
+
+
+
+
+def split_response(response):
+    return [response[i:i + 4096] for i in range(0, len(response), 4096)]
+
+
+
+if __name__ == '__main__':
+    bot.run(DISCORD_BOT_TOKEN)
